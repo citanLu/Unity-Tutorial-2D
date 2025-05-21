cbf674e9d4a059a2030060d119d54673f61ed0d5
JS <ruby527257@gmail.com>
2025-05-21 17:52:41 +0900

    push
    2025.05.21
    - 메모리 저장과 배열
    - 접근 제한자
    - Component

---
+++ Assets/02. Scripts/StudyComponent.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class StudyComponent : MonoBehaviour
+{
+    public GameObject obj; // 큐브 게임 오브젝트를 가져오고 싶다
+
+    public string changeName;
+
+    void Start()
+    {
+        obj = GameObject.Find("Main Camera");
+        // Main Camera 오브젝트를 찾아서 할당하는 기능
+
+        obj.name = changeName;
+    }
+}
