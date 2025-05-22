27fe7d6d257b185155a1294d2c3f407a54165c4b
JS <ruby527257@gmail.com>
2025-05-22 18:26:56 +0900

    push
    2025.05.22
    - 컴포넌트 접근, 활성 & 비활성화
    - 즉시 파괴와 지연 파괴
    - 자식 오브젝트와 부모 오브젝트
    - 캐릭터의 부드러운 이동 (Input System)

---
+++ Assets/02. Scripts/StudyGameobject.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class StudyGameobject : MonoBehaviour
+{
+    public GameObject prefab;
+
+    void Start()
+    {
+        CreateAmongus();
+    }
+
+    void CreateAmongus()
+    {
+        GameObject obj = Instantiate(prefab);
+
+        obj.name = "어몽어스 캐릭터";
+
+        Debug.Log($"캐릭터의 자식 오브젝트 수 : {obj.transform.childCount}");
+
+        Debug.Log($"캐릭터의 첫번째 자식 오브젝트 수 : {obj.transform.GetChild(0).name}");
+
+        Debug.Log($"캐릭터의 마지막 자식 오브젝트 수 : {obj.transform.GetChild(obj.transform.childCount - 1).name}");
+    }
+}
